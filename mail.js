27203377c6ac315ae180139929fdb452077e4739
avi9360 <savinash406@gmail.com>
2022-07-11 19:36:58 +0530

    Removed uplode dir & mail upgraded

--- mail.js
+++ mail.js
@@ -22,7 +22,7 @@ function sendMail(sendTOId, sendSubject, sendText) {
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      return error;
+      throw error;
     } else {
       return info.response;
     }
--- server.js
+++ server.js
@@ -4,21 +4,25 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const { sendMail } = require("./mail");
-
+const redis = require("@redis/client");
+const client = redis.createClient();
+(async () => {
+  await client.connect();
+})();
+client.on("connect", () => console.log("::> Redis Client Connected"));
+client.on("error", err => console.log("<:: Redis Client Error", err));
 // middleware
 app.use(cors());
+app.use(express.json());
 
-app.get("/file/:Key", (req, res) => {
-  const Key = req.params.Key;
-  return res.send({ key });
+app.get("/redis", async (req, res) => {
+  await client.set("key", "value");
+  const value = await client.get("key");
+  res.json(value);
 });
 
-app.post("/file", async function (req, res) {
-  return res.send({ OK: "Success" });
-});
-
-app.post("/send", async function (req, res) {
-  const data = await sendMail("fenig57904@weepm.com", "hey", "ohk");
+app.get("/send", async function (req, res) {
+  const data = await sendMail("savinash406@gmail.com", "hey", "ohk");
   return res.send({ ok: "okk", data });
 });
 
